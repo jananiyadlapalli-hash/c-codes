608a31a49e385000e87aa2cf039dbd8ec366fe27
janani <jananiyadlapalli@gmail.com>
2025-08-25 23:59:05 +0530

    Add files via upload

---
+++ 1 n natural N.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum;
+
+    printf("enter the number: ");
+    scanf("%d", &n);
+
+    sum = n * (n + 1) / 2;
+
+    printf("sum=%d\n", sum);
+
+    return 0;
+}
