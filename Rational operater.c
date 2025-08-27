12c4f08336c25d99c852f6893052e959edb26006
janani <jananiyadlapalli@gmail.com>
2025-08-27 10:19:21 +0530

    Add files via upload

---
+++ Rational operater.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+
+   printf("enter two numbers in x: and y: ");
+   scanf("%d %d", &x, &y);
+
+
+
+    printf("x == y : %d\n", x == y);
+    printf("x != y : %d\n", x != y);
+    printf("x > y  : %d\n", x > y);
+    printf("x < y  : %d\n", x < y);
+    printf("x >= y : %d\n", x >= y);
+    printf("x <= y : %d\n", x <= y);
+    
+    return 0;
+}
