c4f501515a97017ef01a54cd96a09dd547b036fc
janani <jananiyadlapalli@gmail.com>
2025-09-04 15:13:52 +0530

    Update collinear.c

--- collinear.c
+++ collinear.c
@@ -1,24 +1,23 @@
-//WAP to check 3 points (x1,y1), (x2,y2), (x3,y3) are collinear or not
 #include <stdio.h>
-int main ()
-{
+
+int main() {
     int x1,y1,x2,y2,x3,y3;
-    printf("enter the coordinate of 1st p: ");
-    scanf("%d,%d",&x1,&y1);
 
-    printf("enter the coordinate of 2nd p: ");
-    scanf("%d,%d",&x2,&y2);
+    printf("Enter the coordinate of 1st point: ");
+    scanf("%d %d",&x1,&y1);
 
-    printf("enter the coordinate of 3rd p: ");
-    scanf("%d,%d",&x3,&y3);
+    printf("Enter the coordinate of 2nd point: ");
+    scanf("%d %d",&x2,&y2);
 
-    if (((y2-y1)/(x2-x1))==((y2-y1)/(x2-x1)))
-    {
-        printf("points are collinear");
+    printf("Enter the coordinate of 3rd point: ");
+    scanf("%d %d",&x3,&y3);
+
+    // Collinearity condition using area of triangle
+    if (x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2) == 0) {
+        printf("Points are collinear\n");
+    } else {
+        printf("Points are not collinear\n");
     }
-    else
-    {
-        printf("points are not collinear");
-    }
+
     return 0;
 }
